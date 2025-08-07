084dcdcde3d66a6949640b1cb24e4f17840790cf
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2025-08-07 17:50:36 +0300

    Add ToastProvider to main.jsx for enhanced notification management

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import { HeroUIProvider } from '@heroui/react'
+import { HeroUIProvider, ToastProvider } from '@heroui/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <HeroUIProvider>
+      <ToastProvider placement='top-center'/>
       <App />
     </HeroUIProvider>
-  </StrictMode>
+  // </StrictMode>
 )
