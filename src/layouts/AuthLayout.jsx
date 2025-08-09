54b4490ceda7853ffa115209bcef9e3a0fec8105
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2025-08-09 04:32:16 +0300

    Remove unused animated circle components from AuthLayout for cleaner code

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -11,10 +11,6 @@ function AuthLayout() {
                     className="absolute bottom-8 -left-4 w-48 h-48 sm:w-72 sm:h-72 bg-blue-400 dark:bg-blue-500 rounded-full  filter blur-xl opacity-20 animate-pulse"
                     style={{ animationDelay: '2s' }}
                 ></div>
-                {/* <div
-                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-48 h-48 sm:w-72 sm:h-72 bg-indigo-400 dark:bg-indigo-500 rounded-full  filter blur-xl opacity-20 animate-pulse"
-                    style={{ animationDelay: '4s' }}
-                ></div> */}
 
                 {/* Additional animated circles */}
                 <div
@@ -39,25 +35,10 @@ function AuthLayout() {
                     className="absolute top-2/3 left-1/5 w-9 h-9 bg-white rounded-full opacity-40 animate-ping"
                     style={{ animationDelay: '1.5s' }}
                 ></div>
-                {/* <div
-                    className="absolute top-1/5 right-1/5 w-5 h-5 bg-white rounded-full opacity-25 animate-ping"
-                    style={{ animationDelay: '2.8s' }}
-                ></div> */}
                 <div
                     className="absolute top-30 left-20 w-5 h-5 bg-white rounded-full opacity-50 animate-ping"
                     style={{ animationDelay: '0.8s' }}
                 ></div>
-
-                {/* Additional spinning circles */}
-                {/* <div className="absolute top-10 left-10 w-16 h-16 border-2 border-purple-300 dark:border-purple-400 rounded-full opacity-20 animate-spin"></div> */}
-                {/* <div
-                    className="absolute bottom-20 right-20 w-12 h-12 border border-blue-300 dark:border-blue-400 rounded-full opacity-30 animate-spin"
-                    style={{ animationDelay: '1s', animationDuration: '3s' }}
-                ></div> */}
-                {/* <div
-                    className="absolute top-40 right-40 w-8 h-8 border border-indigo-300 dark:border-indigo-400 rounded-full opacity-25 animate-spin"
-                    style={{ animationDelay: '2s', animationDuration: '4s' }}
-                ></div> */}
             </div>
 
             <Outlet />
