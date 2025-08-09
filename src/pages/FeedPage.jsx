8a933f61427b82d57e24e7d141d67ce16aaa73c4
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2025-08-09 16:31:08 +0300

    Refactor FeedPage component by commenting out unused Avatar and img elements for improved code clarity and potential future use.

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -32,15 +32,15 @@ function FeedPage() {
     }, [])
 
     return (
-        <Card isBlurred>
+        <Card isBlurred isPressable className="w-full">
             <CardHeader>
                 <div className="flex gap-5 cursor-pointer">
-                    <Avatar
+                    {/* <Avatar
                         isBordered
                         radius="full"
                         size="md"
                         src="https://heroui.com/avatars/avatar-1.png"
-                    />
+                    /> */}
                     <div className="flex flex-col gap-1 items-start justify-center">
                         <h4 className="text-small font-semibold leading-none text-default-600">Zoey Lang</h4>
                         <h5 className="text-small tracking-tight text-default-400">@zoeylang</h5>
@@ -62,10 +62,10 @@ function FeedPage() {
                     className="object-cover rounded-xl"
                     src="https://heroui.com/images/hero-card-complete.jpeg"
                     /> */}
-                <img 
+                {/* <img 
                     className="object-cover rounded-xl mx-auto"
                     src="https://heroui.com/images/hero-card-complete.jpeg" 
-                    alt="Card background" />
+                    alt="Card background" /> */}
             </CardBody>
         </Card>
     )
