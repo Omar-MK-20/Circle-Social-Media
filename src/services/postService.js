5d8affea45ebe86438dac0b9430ef2bf4de251bf
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2025-08-09 04:33:43 +0300

    Add postService to handle API interactions for fetching posts

---
+++ src/services/postService.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const baseUrl = 'https://linked-posts.routemisr.com/posts';
+
+
+
+export const postApi =
+{
+    getAll: async () => 
+    {
+        try 
+        {
+            const { data } = await axios.get(baseUrl, {
+                headers:
+                {
+                    token: localStorage.getItem('token')
+                }}
+            )        
+            return data
+
+        } 
+        catch (error) 
+        {
+            return error.response.data;
+    
+        }
+    }
+}
