8edf9eef93826d73ce449bcf3d08eddae9804e0d
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2025-08-09 04:32:07 +0300

    Refactor NavbarComponent to implement a responsive navigation bar with dropdown profile actions

--- src/components/NvabarComponent.jsx
+++ src/components/NvabarComponent.jsx
@@ -1,10 +1,79 @@
+import { Link } from 'react-router-dom';
+import avatar from '../assets/avatar.png'
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+  Avatar,
+} from "@heroui/react";
+
 
 function NvabarComponent() {
   return (
-    <div>
-        <h1 className="text-2xl">NvabarComponent</h1>
-    </div>
-  )
+    <Navbar 
+      shouldHideOnScroll
+      className="rounded-b-xl fixed"
+      >
+      <NavbarBrand>
+        <Link to={'/'} className="font-bold text-inherit">CIRLCE</Link>
+      </NavbarBrand>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link href="#">
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent as="div" justify="end">
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger className='cursor-pointer'>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="secondary"
+              name="Jason Hughes"
+              size="sm"
+              src={avatar}
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">zoey@example.com</p>
+            </DropdownItem>
+            <DropdownItem key="settings">My Settings</DropdownItem>
+            <DropdownItem key="team_settings">Team Settings</DropdownItem>
+            <DropdownItem key="analytics">Analytics</DropdownItem>
+            <DropdownItem key="system">System</DropdownItem>
+            <DropdownItem key="configurations">Configurations</DropdownItem>
+            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+            <DropdownItem key="logout" color="danger">
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarContent>
+    </Navbar>
+  );
 }
+
 
 export default NvabarComponent
