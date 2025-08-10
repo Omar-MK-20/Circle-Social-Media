9362d820ace2f59ea2ec3f500b1c8b728cade38a
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2025-08-10 18:34:10 +0300

    Refactor MainProtRoute to use AuthContext for authentication, replacing localStorage check with context-based login status for improved state management.

--- src/protectetdRoutes/MainProtRoute.jsx
+++ src/protectetdRoutes/MainProtRoute.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Navigate } from 'react-router-dom'
+import { AuthContext } from '../contexts/AuthContextProvider'
 
 function MainProtRoute({ children }) {
-    const isLoggedIn = !!localStorage.getItem('token')
+    // const isLoggedIn = !!localStorage.getItem('token')
+    const { isLoggedIn } = useContext(AuthContext);
     return isLoggedIn ? children : <Navigate to={'/login'} />
 }
 
