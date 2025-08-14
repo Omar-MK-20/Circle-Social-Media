cf72870c91abc24e4134bc4fd8e987c8dac43277
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2025-08-14 17:43:24 +0300

    Implement PostDetailsPage with post fetching and loading state management

--- src/pages/PostDetailsPage.jsx
+++ src/pages/PostDetailsPage.jsx
@@ -1,10 +1,70 @@
+import { useParams } from "react-router-dom"
+import { postApi } from "../services/postService"
+import { useEffect, useState } from "react";
+import { addToast, Modal, ModalBody, ModalContent, useDisclosure } from "@heroui/react";
+import LoadingPostComponent from "../components/LoadingPostComponent";
+import PostComponent from "../components/Post/PostComponent";
 
 function PostDetailsPage() {
-  return (
-    <div>
-        <h1 className="text-2xl">PostDetailsPage</h1>
-    </div>
-  )
+    const { id } = useParams();
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    const [post, setPost] = useState(null);
+    const [viewImgSrc, setViewImgSrc] = useState(null);
+    const [isLoadingPost, setIsLoadingPost] = useState(true);
+
+
+    async function getOnePost() {
+        const data = await postApi.getOne(id);
+        if (data.error) {
+            addToast(
+                {
+                    title: "Failed getting Post",
+                    description: data.error,
+                    color: 'danger',
+                }
+            )
+            setIsLoadingPost(false);
+            return
+        }
+        setIsLoadingPost(false);
+        setPost(data.post);
+
+    }
+
+    useEffect(
+        () => {
+            getOnePost();
+        }, []
+    )
+
+
+    return (
+        <>
+            <div className="w-full max-w-2xl backdrop-blur-md rounded-xl sm:p-6 space-y-7 relative">
+
+                {
+                    isLoadingPost ? <LoadingPostComponent /> :
+                    post && <PostComponent getData={getOnePost} post={post} onOpen={onOpen} setViewImgSrc={setViewImgSrc} numOfComments={undefined} />
+                }
+
+
+                <Modal isOpen={isOpen} size="xl" placement="center" scrollBehavior="outside" onOpenChange={onOpenChange}>
+                    <ModalContent>
+                        {() => (
+                            <>
+                                <ModalBody>
+                                    <img
+                                        className="rounded-xl mx-auto w-full object-cover"
+                                        src={viewImgSrc}
+                                        alt="Post Photo" />
+                                </ModalBody>
+                            </>
+                        )}
+                    </ModalContent>
+                </Modal>
+            </div>
+        </>
+    )
 }
 
 export default PostDetailsPage
