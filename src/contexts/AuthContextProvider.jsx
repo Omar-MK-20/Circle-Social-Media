3e32bd625ad340dd9b96793727b3215b4268fee3
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2025-08-14 17:42:45 +0300

    Add user data handling in AuthContextProvider for improved user profile management

--- src/contexts/AuthContextProvider.jsx
+++ src/contexts/AuthContextProvider.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { userApi } from "../services/userService";
 
 
 export const AuthContext = createContext();
@@ -6,10 +7,37 @@ export const AuthContext = createContext();
 
 export default function AuthContextProvider({children})
 {
-    const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('token'))
+    const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('token'));
+    const [ userData, setUserData ] = useState(null);
+
+
+    async function handleUserData() {
+            const data = await userApi.getUserProfile();
+            if (data.error) {
+                setUserData({
+                    name: "",
+                    email: "Retry again",
+                    photo: "https://linked-posts.routemisr.com/uploads/default-profile.png",
+                })
+                return
+            }
+            setUserData(data.user);
+        }
+    
+    
+        useEffect( () => 
+        {
+            if (isLoggedIn)
+            {
+                handleUserData();
+            }
+    
+        }, [isLoggedIn])
+
+
 
     return(
-        <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+        <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, userData, setUserData }}>
             {children}
         </AuthContext.Provider>
     )
