593f164b4177c578f53bfd423311144cc655cff6
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2025-08-18 01:35:49 +0300

    Enhance AddPostComponent with post creation functionality, error handling, and user feedback

--- src/components/Post/AddPostComponent.jsx
+++ src/components/Post/AddPostComponent.jsx
@@ -1,24 +1,104 @@
-import { Button, Card, CardHeader, Form, Input, Textarea } from "@heroui/react"
-import { useState } from "react";
-import avatar from '../../assets/avatar.png'
+import { addToast, Button, Card, CardHeader, Form, Input, Textarea } from "@heroui/react";
+import { useMutation } from "@tanstack/react-query";
+import { useEffect, useRef, useState } from "react";
+import { postApi } from "../../services/postService";
 
-function AddPostComponent() {
+function AddPostComponent({ refetch, isFetching }) {
 
     const [isCreatingPost, setIsCreatingPost] = useState(false);
-    const [imgSrc, setImgSrc] = useState(avatar)
+    const [imgSrc, setImgSrc] = useState(null);
+    const [caption, setCaption] = useState('');
+    const [postImage, setPostImage] = useState(undefined);
+    const [formData, setFormData] = useState(null);
+    const postImgElement = useRef('');
 
-    function handleCreatingPost()
-    {
-        if(isCreatingPost == true)
+    const { mutate, isPending, isError, error, isSuccess } = useMutation(
         {
-            setIsCreatingPost(false);
-            setImgSrc(avatar);
+            mutationFn: () => postApi.createOne(formData)
         }
-        else
-        {
+    )
+
+    function handleClosingOpeningForm() {
+        if (isCreatingPost == true) {
+            clearPostForm();
+        }
+        else {
             setIsCreatingPost(true);
         }
     }
+
+    function clearPostForm() {
+        setIsCreatingPost(false);
+        setImgSrc(null);
+        setPostImage(undefined);
+        setCaption('');
+        postImgElement.current.value = '';
+    }
+
+
+
+    function handleViewPostImg(e) {
+        if (e.target.files[0]) {
+            setPostImage(e.target.files[0])
+            const src = URL.createObjectURL(e.target.files[0])
+            setImgSrc(src);
+        }
+
+    }
+
+
+    function handleCreatePost() {
+
+        if ((caption.trim().length <= 2) && !postImage) {
+            return
+        }
+
+        const formData = new FormData();
+
+        if (!(caption.trim().length <= 2)) {
+            formData.append('body', caption);
+        }
+        if (postImage) {
+            formData.append('image', postImage);
+        }
+        console.log(formData);
+        setFormData(formData);
+        mutate();
+    }
+
+
+    
+
+    async function errorHandling()
+    {
+        if (isError) {
+            addToast(
+                {
+                    title: "Sharing post failed",
+                    description: error.message,
+                    color: 'danger',
+                }
+            )
+        }
+
+        if (isSuccess) {
+            await refetch()
+            clearPostForm();
+            addToast(
+                {
+                    title: 'Post shared Successfully',
+                    color: 'success'
+                }
+            )
+            
+        }
+    }
+
+    useEffect(() => {
+        errorHandling();
+    }, [isError, isSuccess])
+
+
 
     return (
         <>
@@ -27,23 +107,29 @@ function AddPostComponent() {
                     {isCreatingPost ?
                         <Form className="w-full">
                             <div className="w-full">
-                                <Textarea placeholder="What's on your mind? Add a Post..." autoFocus={true} />
+                                <Textarea value={caption} onChange={(e) => setCaption(e.target.value)} placeholder="What's on your mind? Add a Post..." autoFocus={true} />
                             </div>
                             {imgSrc &&
                                 <div className="w-full relative">
-                                    <img src={imgSrc} alt="" className="w-full h-80 object-cover" />
-                                    <button onClick={() => setImgSrc(null)} type="button" className="bg-default-300 absolute top-0 end-0 w-6 h-6 m-1 rounded-full cursor-pointer" ><i className="fa-solid fa-xmark"></i></button>
+                                    <img src={imgSrc} alt="" className="w-full h-80 object-cover rounded-md" />
+                                    <button onClick={() => { postImgElement.current.value = ''; setPostImage(undefined); setImgSrc(null) }} type="button" className="bg-default-300 absolute top-0 end-0 w-6 h-6 m-1 rounded-full cursor-pointer" ><i className="fa-solid fa-xmark"></i></button>
                                 </div>}
                             <div className="flex justify-between w-full gap-2">
-                                <Input className="w-fit" type="file"/>
+                                <Input
+                                    ref={postImgElement}
+                                    onChange={handleViewPostImg}
+                                    className="w-fit"
+                                    type="file"
+                                    accept=".jpg,.jpeg,.png"
+                                />
                                 <div className="flex justify-between gap-1">
-                                    <Button onPress={handleCreatingPost} color="warning" variant="flat">Cancel</Button>
-                                    <Button color="primary">Post</Button>
+                                    <Button onPress={handleClosingOpeningForm} color="warning" variant="flat">Cancel</Button>
+                                    <Button isLoading={isPending || isFetching} isDisabled={!postImage && caption.trim().length <= 2} onPress={handleCreatePost} color="primary">Post</Button>
                                 </div>
                             </div>
                         </Form>
                         :
-                        <Button onPress={handleCreatingPost} className="w-full justify-start">
+                        <Button onPress={handleClosingOpeningForm} className="w-full justify-start">
                             What's on your mind? Add a Post...
                         </Button>
                     }
