cc59e82ff7de94c10e50fe36006961d56bbe15f1
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2025-08-20 15:54:08 +0300

    Enhance CommentComponent: Add edit and delete options for comments, utilizing Dropdown for user actions

--- src/components/CommentComponent.jsx
+++ src/components/CommentComponent.jsx
@@ -1,15 +1,32 @@
-import { Avatar, CardBody, CardFooter, Divider } from '@heroui/react';
+import { Avatar, Button, CardBody, CardFooter, Divider, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from '@heroui/react';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import avatar from '../assets/avatar.png';
+import { useContext } from 'react';
+import { AuthContext } from '../contexts/AuthContextProvider';
 
-function CommentComponent({ comments, numOfComments }) {
+function CommentComponent({ comments, numOfComments, postUser }) {
     dayjs.extend(relativeTime);
+    const { userData } = useContext(AuthContext);
 
     return (
         <>
-            {comments.slice(0, numOfComments).map( (comment) => (
+            {comments.slice(0, numOfComments).map((comment) => (
                 <CardFooter key={comment._id} className="px-8 flex flex-col items-start gap-3 mx-auto h-fit sm:my-1">
+                    {userData?._id == postUser._id &&
+                        <Dropdown>
+                            <DropdownTrigger>
+                                <Button color='light' className='absolute top-0 end-0 m-4 p-2'>
+                                    <i className="fa-solid fa-ellipsis text-2xl"></i>
+                                </Button>
+                            </DropdownTrigger>
+                            <DropdownMenu aria-label="Static Actions" variant="shadow">
+                                <DropdownItem key="edit">Edit</DropdownItem>
+                                <DropdownItem key="delete" className="text-danger" color="danger">
+                                    Delete
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </Dropdown>}
                     <div className="flex gap-5 cursor-pointer w-fit">
                         <Avatar
                             isBordered
@@ -25,9 +42,9 @@ function CommentComponent({ comments, numOfComments }) {
                     <CardBody className="px-3 py-0 text-medium text-default-700 overflow-visible">
                         <p className="text-sm">{comment.content}</p>
                     </CardBody>
-                    <Divider/>
+                    <Divider />
                 </CardFooter>
-            ) )
+            ))
             }
         </>
     )
