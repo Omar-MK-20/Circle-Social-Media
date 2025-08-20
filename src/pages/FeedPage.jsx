e3d286ab2c70ce4b9f1412e24f45a6a24ebe47ac
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2025-08-20 15:56:36 +0300

    Refactor FeedPage: Integrate UpdatePostComponent for post editing, enhance scrolling behavior for loading more posts, and clean up unused code and comments.

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -2,7 +2,7 @@ import { addToast, Button, Modal, ModalBody, ModalContent, useDisclosure } from 
 import { useInfiniteQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
 import relativeTime from 'dayjs/plugin/relativeTime';
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { queryClient } from "../App";
 import LoadingPostComponent from "../components/LoadingPostComponent";
@@ -10,6 +10,7 @@ import AddPostComponent from "../components/Post/AddPostComponent";
 import PostComponent from "../components/Post/PostComponent";
 import { AuthContext } from "../contexts/AuthContextProvider";
 import { postApi } from "../services/postService";
+import UpdatePostComponent from "../components/Post/UpdatePostComponent";
 
 
 function FeedPage() {
@@ -17,75 +18,19 @@ function FeedPage() {
     dayjs.extend(relativeTime);
     const navigate = useNavigate();
     const { setIsLoggedIn } = useContext(AuthContext);
+
+    // useDisclosure and state for update post
+    const updataPostDisclosure = useDisclosure();
+    const [ postDetailsForEdit, setPostDetailsForEdit ] = useState({})
+
+
+
+
+    // useDisclosure and state for view image
     const { isOpen, onOpen, onOpenChange } = useDisclosure();
     const [viewImgSrc, setViewImgSrc] = useState(null);
+    const scrollingElement = useRef({});
     
-    // const [posts, setPosts] = useState([]);
-    // const [isLoadingPost, setIsLoadingPost] = useState(true);
-    // const [isFetchingNextPage, setIsFetchingNextPage] = useState(false)
-    // const [error, setError] = useState(null)
-
-
-    // async function getAllPosts() {
-    //     setError(null)
-    //     // setIsLoadingPost(true);
-    //     const data = await postApi.getAll();
-    //     console.log("🚀 ~ getPosts ~ data:", data);
-
-    //     if (data.error) {
-    //         console.log("🚀 ~ getAllPosts ~ data.error:", data.error)
-    //         console.log(data.error);
-    //         console.log("invalid token", data.error.includes("invalid token"));
-    //         console.log("JsonWebTokenError", data.error.includes("JsonWebTokenError"));
-
-
-    //         if (data.error.includes("JsonWebTokenError") || data.error.includes("invalid token")) {
-    //             addToast(
-    //                 {
-    //                     title: "Feed loading failed",
-    //                     description: 'Login again',
-    //                     color: 'danger',
-    //                 })
-    //             navigate('/login', { viewTransition: true })
-    //             localStorage.removeItem('token');
-    //             setIsLoggedIn(false);
-    //             return
-
-    //         }
-    //         addToast(
-    //             {
-    //                 title: "Feed loading failed",
-    //                 description: data.error,
-    //                 color: 'danger',
-    //             }
-    //         )
-
-
-    //         setError('something went wrong')
-    //     }
-
-    //     setIsLoadingPost(false);
-    //     setPosts(data.posts);
-    //     return data;
-
-    // }
-
-    // useEffect(() => {
-    //     getAllPosts()
-    // }, [])
-
-
-    // const { data: { posts = [] } = {}, isLoading, isError, error } = useQuery({
-    //     queryKey: ['posts'],
-    //     queryFn: postApi.getAll,
-    //     select: (data) => data.data,
-    //     staleTime: 5 * 1000,
-    // })
-
-
-
-
-
 
 
 
@@ -94,15 +39,31 @@ function FeedPage() {
         queryFn: (data) => { return postApi.getAll(data.pageParam); },
         initialPageParam: 1,
         getNextPageParam: (lastPage) => {
-            // console.log(lastPage.data.paginationInfo.nextPage)
             if (lastPage.data.paginationInfo.nextPage) {
                 return lastPage.data.paginationInfo.nextPage;
             }
             return undefined;
         },
-        staleTime: 1000 * 30 * 1
-
+        staleTime: 1000 * 10 * 1,
     })
+
+
+    async function handleFetchNextPage() {
+        const { clientHeight, scrollHeight } = scrollingElement.current;
+        const currentScroll = scrollHeight - clientHeight - 225;
+        await fetchNextPage();
+        window.scrollTo(
+            {
+                top: currentScroll,
+                behavior: 'smooth'
+            }
+        );
+    }
+
+    function handleScrolling(e) {
+        const { clientHeight, scrollTop, scrollHeight } = e.target.scrollingElement;
+        scrollingElement.current = { clientHeight, scrollTop, scrollHeight }
+    }
 
 
     useEffect(() => {
@@ -132,10 +93,6 @@ function FeedPage() {
         }
     }, [isError])
 
-    // console.log(pages)
-
-
-
 
 
     // async function handleScroll(e) 
@@ -147,18 +104,23 @@ function FeedPage() {
     //     // console.log(clientHeight+scrollTop);
     //     console.log('+++++++++++++++++++++++++++++++++++++++++++++')
 
-    //     if(scrollHeight-100 <= clientHeight+scrollTop)
+    //     if(scrollHeight-100 <= clientHeight+scrollTop && !isFetchingNextPage)
     //     {
     //         // console.log('scrollHeight-100 <= clientHeight+scrollTop');
     //         // console.log('=======================================')
-    //         console.log("🚀 ~ handleScroll ~ isFetchingNextPage: before", isFetchingNextPage)
-    //         if(!isFetchingNextPage)
+    //         // console.log("🚀 ~ handleScroll ~ isFetchingNextPage: before", isFetchingNextPage)
+    //         // console.log(isFetching);
+    //         // console.log(hasNextPage);
+
+    //         console.log("🚀 ~ handleScroll ~ isFetching:", isFetching)
+    //         if(!isFetching)
     //         {
-    //             setIsFetchingNextPage(true)
-    //             await fetchNextPage()
+    //             // setIsFetchingNextPage(true)
+                
+    //             await handleFetchNextPage()
     //             // setIsFetchingNextPage(false)
     //         }
-    //         console.log("🚀 ~ handleScroll ~ isFetchingNextPage: after", isFetchingNextPage)
+    //         // console.log("🚀 ~ handleScroll ~ isFetchingNextPage: after", isFetchingNextPage)
 
 
     //     }
@@ -168,13 +130,19 @@ function FeedPage() {
     // handleScroll()
 
 
-    // useEffect(() => {
-    //     document.addEventListener('scroll', handleScroll)
 
-    //     return () => {
-    //         document.removeEventListener('scroll', handleScroll)
-    //     }
-    // }, [])
+
+
+
+    useEffect(() => {
+        // document.addEventListener('scroll', handleScroll)
+        document.addEventListener('scroll', handleScrolling)
+
+        return () => {
+            // document.removeEventListener('scroll', handleScroll)
+            document.removeEventListener('scroll', handleScrolling)
+        }
+    }, [])
 
 
 
@@ -184,7 +152,6 @@ function FeedPage() {
                 className="w-full max-w-2xl backdrop-blur-md rounded-xl sm:p-6 space-y-7 relative">
 
                 <AddPostComponent refetch={refetch} isFetching={isFetching} />
-
                 {
                     isLoading
                         ?
@@ -198,21 +165,24 @@ function FeedPage() {
                                 <div key={index} className="space-y-7">
                                     {
                                         page.data.posts.map((post) => (
-                                            <PostComponent getData={refetch} key={post._id} post={post} onOpen={onOpen} setViewImgSrc={setViewImgSrc} numOfComments={1} />
+                                            <PostComponent getData={refetch} key={post._id} post={post} onOpen={onOpen} setViewImgSrc={setViewImgSrc} numOfComments={1} setPostDetails={setPostDetailsForEdit} updataPostDisclosure={updataPostDisclosure} />
                                         ))
                                     }
                                 </div>
                             ))
-
-
                 }
+
 
                 {!isError &&
                     <div className="w-full">
-                        <Button className="w-full" isLoading={isFetchingNextPage} isDisabled={!hasNextPage} onPress={fetchNextPage}>load more...</Button>
+                        <Button className="w-full" isLoading={isFetchingNextPage} isDisabled={!hasNextPage} onPress={handleFetchNextPage}>load more...</Button>
                     </div>}
 
 
+
+
+
+                <UpdatePostComponent postIsOpen={updataPostDisclosure.isOpen} postOnOpenChange={updataPostDisclosure.onOpenChange} postDetails={postDetailsForEdit} queryKey={'posts'} />
 
                 <Modal backdrop="blur" isOpen={isOpen} size="xl" placement="center" scrollBehavior="outside" onOpenChange={onOpenChange}>
                     <ModalContent>
@@ -228,6 +198,8 @@ function FeedPage() {
                         )}
                     </ModalContent>
                 </Modal>
+
+
 
 
             </div>
