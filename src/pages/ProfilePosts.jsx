2b44fc05258c193290974065652171f3a9928656
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2025-08-24 01:39:29 +0300

    Add ProfilePosts component: Implement user-specific post display with loading and error handling, along with functionality for adding and updating posts. Integrate modal for image viewing to enhance user interaction.

---
+++ src/pages/ProfilePosts.jsx
@@ -0,0 +1,78 @@
+import { useQuery } from "@tanstack/react-query"
+import { useParams } from "react-router-dom"
+import { userApi } from "../services/userService";
+import AddPostComponent from "../components/Post/AddPostComponent";
+import LoadingPostComponent from "../components/LoadingPostComponent";
+import PostComponent from "../components/Post/PostComponent";
+import { Modal, ModalBody, ModalContent, useDisclosure } from "@heroui/react";
+import { useState } from "react";
+import UpdatePostComponent from "../components/Post/UpdatePostComponent";
+
+
+
+
+function ProfilePosts() {
+
+    const { id } = useParams();
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    const [viewImgSrc, setViewImgSrc] = useState(null);
+
+    // useDisclosure and state for update post
+    const updataPostDisclosure = useDisclosure();
+    const [ postDetailsForEdit, setPostDetailsForEdit ] = useState({})
+
+
+
+    const { data: { posts = [] } = {}, isLoading, isError, error, refetch, isFetching } = useQuery(
+        {
+            queryKey: ['userPosts'],
+            queryFn: () => userApi.getUserPosts(id),
+            select: (data) => data.data,
+            staleTime: 1000 * 30 * 1
+        }
+    )
+
+
+    return (
+        <div className="w-full max-w-2xl backdrop-blur-md rounded-xl sm:p-6 space-y-7 relative">
+            <AddPostComponent refetch={refetch} isFetching={isFetching} />
+
+
+            {
+                isLoading
+                    ?
+                    <LoadingPostComponent />
+                    :
+                    isError
+                        ?
+                        <div className="w-full text-center bg-red-300/50 text-red-600 py-4  rounded-md ">{error.message}</div>
+                        :
+                        posts.map((post) => (
+                            <PostComponent getData={refetch} key={post._id} post={post} onOpen={onOpen} setViewImgSrc={setViewImgSrc} numOfComments={1} setPostDetails={setPostDetailsForEdit} updataPostDisclosure={updataPostDisclosure} />
+                        ))
+            }
+
+
+            <UpdatePostComponent postIsOpen={updataPostDisclosure.isOpen} postOnOpenChange={updataPostDisclosure.onOpenChange} postDetails={postDetailsForEdit} queryKey={'userPosts'} />
+
+            <Modal backdrop="blur" isOpen={isOpen} size="xl" placement="center" scrollBehavior="outside" onOpenChange={onOpenChange}>
+                <ModalContent>
+                    {() => (
+                        <>
+                            <ModalBody>
+                                <img
+                                    className="rounded-xl mx-auto w-full object-cover"
+                                    src={viewImgSrc}
+                                    alt="Post Photo" />
+                            </ModalBody>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
+
+
+        </div>
+    )
+}
+
+export default ProfilePosts
