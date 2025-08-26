f8c395386ccd6bdd8555db7fe819bda4c5b04238
Omar-MK-20 <omar.mk.201@gmail.com>
2025-08-26 17:32:56 +0300

    Add editing functionality to comments: Implement state management for editing comments, allowing users to modify their comments with a save and cancel option.

--- src/components/CommentComponent.jsx
+++ src/components/CommentComponent.jsx
@@ -1,19 +1,34 @@
-import { Avatar, Button, CardBody, CardFooter, Divider, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from '@heroui/react';
+import { Avatar, Button, CardBody, CardFooter, Divider, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Input } from '@heroui/react';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import avatar from '../assets/avatar.png';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../contexts/AuthContextProvider';
 
 function CommentComponent({ comments, numOfComments, postUser }) {
     dayjs.extend(relativeTime);
     const { userData } = useContext(AuthContext);
+    const [isEditing, setIsEditing] = useState(false);
+    const [commentContent, setCommentContent] = useState(null);
+
+    function handleIsEditingComment(content) {
+        if (isEditing) {
+            setIsEditing(false)
+        }
+        else {
+            setIsEditing(true)
+            setCommentContent(content)
+        }
+    }
 
     return (
         <>
             {comments.slice(0, numOfComments).map((comment) => (
                 <CardFooter key={comment._id} className="px-8 flex flex-col items-start gap-3 mx-auto h-fit sm:my-1">
-                    {userData?._id == postUser._id &&
+                    <p className='m-0'>{userData?._id}</p>
+                    <p className='m-0'>{comment.commentCreator._id}</p>
+
+                    {(userData?._id == comment.commentCreator._id) &&
                         <Dropdown>
                             <DropdownTrigger>
                                 <Button color='light' className='absolute top-0 end-0 m-4 p-2'>
@@ -21,7 +36,7 @@ function CommentComponent({ comments, numOfComments, postUser }) {
                                 </Button>
                             </DropdownTrigger>
                             <DropdownMenu aria-label="Static Actions" variant="shadow">
-                                <DropdownItem key="edit">Edit</DropdownItem>
+                                <DropdownItem onPress={() => handleIsEditingComment(comment.content)} key="edit">Edit</DropdownItem>
                                 <DropdownItem key="delete" className="text-danger" color="danger">
                                     Delete
                                 </DropdownItem>
@@ -39,9 +54,19 @@ function CommentComponent({ comments, numOfComments, postUser }) {
                             <h5 className="text-small tracking-tight text-default-400">{dayjs(comment.createdAt).fromNow()}</h5>
                         </div>
                     </div>
-                    <CardBody className="px-3 py-0 text-medium text-default-700 overflow-visible">
-                        <p className="text-sm">{comment.content}</p>
-                    </CardBody>
+                    {isEditing
+                        ?
+                        <CardBody>
+                            <Input value={commentContent} onChange={(e) => setCommentContent(e.target.value)} />
+                            <div className='flex ms-auto pt-2 space-x-2'>
+                                <Button color="warning" variant="flat" onPress={handleIsEditingComment}>Cancel</Button>
+                                <Button color="primary" >Save</Button>
+                            </div>
+                        </CardBody>
+                        :
+                        <CardBody className="px-3 py-0 text-medium text-default-700 overflow-visible">
+                            <p className="text-sm">{comment.content}</p>
+                        </CardBody>}
                     <Divider />
                 </CardFooter>
             ))
