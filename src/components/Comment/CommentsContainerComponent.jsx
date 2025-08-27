0d07251b5a2de979b1ec311c3ea42fe7486bb0fc
Omar-MK-20 <omar.mk.201@gmail.com>
2025-08-27 18:21:08 +0300

    Add CommentsContainerComponent: Create a new component to display a list of comments with integrated CommentComponent.

---
+++ src/components/Comment/CommentsContainerComponent.jsx
@@ -0,0 +1,20 @@
+import { Card } from '@heroui/react';
+import CommentComponent from './CommentComponent';
+
+function CommentsContainerComponent({ getData, comments, numOfComments, postUser }) {
+
+
+    return (
+        <>
+            {comments.slice(0, numOfComments).map((comment) => (
+                <Card isBlurred key={comment._id} className="px-8 py-4 flex flex-col items-start gap-3 mx-auto h-fit sm:my-1 w-auto rounded-none shadow-none">
+
+                    <CommentComponent postUser={postUser} getData={getData} comment={comment} />
+                </Card>
+            ))
+            }
+        </>
+    )
+}
+
+export default CommentsContainerComponent
